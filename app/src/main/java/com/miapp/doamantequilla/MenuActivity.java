726a6dc3bddbd1e0412738aaadd1f9ab4215e1fd
Misael <misael.oyarzun@gmail.com>
2025-10-14 09:20:50 -0300

    Nuevo layout y agreagrar ubiacion de map

--- app/src/main/java/com/miapp/doamantequilla/MenuActivity.java
+++ app/src/main/java/com/miapp/doamantequilla/MenuActivity.java
@@ -8,6 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+
 public class MenuActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +25,25 @@ public class MenuActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Referenciamos el btn
+        Button btnMap = findViewById(R.id.map);
+
+        btnMap.setOnClickListener(v -> {
+            // coordenadas
+            double latitud = -33.35557468918811;
+            double longitud = -70.53296987503704;
+
+            // Crear el intent con la ubicación
+            Uri ubicacion = Uri.parse("geo:" + latitud + "," + longitud + "?q=" + latitud + "," + longitud + "(Mi+Ubicación)");
+            Intent intent = new Intent(Intent.ACTION_VIEW, ubicacion);
+            intent.setPackage("com.google.android.apps.maps");
+
+            // Verificar que haya una app que lo maneje
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
         });
     }
 }
